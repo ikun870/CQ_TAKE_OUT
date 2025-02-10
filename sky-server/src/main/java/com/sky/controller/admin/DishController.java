a4663edecee77bf860f8b64db195f1827fbaa604
ikun870 <pro8720@outlook.com>
2025-02-10 21:36:40 +0800

    使用redis缓存菜品

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -10,9 +10,11 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/admin/dish")
@@ -21,6 +23,9 @@ import java.util.List;
 public class DishController {
     @Autowired
     private DishService dishService;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
     /**
      * 新增菜品
      *
@@ -31,6 +36,9 @@ public class DishController {
     public Result save(@RequestBody DishDTO dishDTO) {
         log.info("新增菜品：{}", dishDTO);
         dishService.saveWithFlavor(dishDTO);
+
+        //删除redis中的菜品缓存
+        deleteRedisCache("dish_"+dishDTO.getCategoryId());
         return Result.success();
     }
 
@@ -59,6 +67,13 @@ public class DishController {
     public Result delete(@RequestParam List<Long> ids){
         log.info("删除菜品：{}", ids);
         dishService.deleteBatch(ids);
+        //删除redis中的菜品缓存
+        //ids.forEach(id -> redisTemplate.delete("dish_" + id));
+        //上面这种写法是错误的，因为id是菜品id，不是分类id
+        //干脆直接删除所有菜品缓存
+        //redisTemplate.delete("dish_*");
+        //上面这种写法是错误的，redisTemplate.delete 方法接收的参数是具体的 Redis key，而不是带有通配符的表达式
+        deleteRedisCache("dish_*");
         return Result.success();
     }
 
@@ -86,7 +101,33 @@ public class DishController {
     public Result update(@RequestBody DishDTO dishDTO) {
         log.info("修改菜品：{}", dishDTO);
         dishService.updateWithFlavor(dishDTO);
+        deleteRedisCache("dish_*");
+
         return Result.success();
+    }
+
+    /**
+     * 菜品起售、停售
+     * @param status
+     * @param id
+     * @return
+     *
+     */
+    @PostMapping("/status/{status}")
+    @ApiOperation(value = "菜品起售、停售")
+    public Result updateStatus(@PathVariable Integer status,Long id) {
+        dishService.startOrStop(status,id);
+        deleteRedisCache("dish_*");
+        return Result.success();
+    }
+
+    /**
+     * 删除redis缓存
+     * @param pattern
+     */
+    private void deleteRedisCache(String pattern) {
+        Set keys = redisTemplate.keys(pattern);
+        redisTemplate.delete(keys);
     }
 
 }
